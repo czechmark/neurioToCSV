80401ea4d6e88fcf6e6b45ccd26ebf7542ec55bf
mark <mark@petschek.com>
2015-06-20 13:31:31 -0400

    my keys

---
+++ my_keys.py
@@ -0,0 +1,6 @@
+key    = "MyNuerioClientIDGoesHere"
+secret = "MyNuerioClientSecretGoesHere"
+# you will have to determine your Sensor ID using the neurioToCSV python script
+sensor_id = "0x0000CAFE5101934B"
+location_id = "CAFEBABEQOutKEz2sUBLEg"
+
