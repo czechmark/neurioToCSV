37b7dab905d5591b3b08f5ab0e0caa36a8930c7e
czechmark <mark@petschek.com>
2015-06-20 09:47:24 -0400

    Delete my_keys.py

--- my_keys.py
+++
@@ -1,6 +0,0 @@
-key    = "MyNuerioClientIDGoesHere"
-secret = "MyNuerioClientSecretGoesHere"
-# you will have to determine your Sensor ID using the neurioToCSV python script
-sensor_id = "0x0000CAFE5101934B"
-location_id = "5DKfZqPwQOutKEz2sUBLEg"
-
